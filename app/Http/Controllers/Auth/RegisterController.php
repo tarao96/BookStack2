66edb4cf5225b1f3117a9632b002bc8a3dc87b73
skeito <s.keito896@gmail.com>
2022-01-22 18:47:52 +0900

    ファイル画像名の保存方法改善

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        $filename = request()->file('user_image');
+        $filename = request()->file('user_image')->getClientOriginalName();
         $path = Storage::disk('s3')->putFile('/', $filename, 'public');
         $data['user_image'] = Storage::disk('s3')->url($path); 
 
